63a330587276762a23679b6bffe2f08cf4f22f06
Vanessa McHale <vamchale@gmail.com>
2020-01-01 13:36:21 -0600

    Tune benchmarks

--- harness.py
+++ harness.py
@@ -1,10 +1,11 @@
 import timeit
 
 setup_fut = """
-import numpy as np
 import information
 
 mod = information.information()
+
+import numpy as np
 
 pre_normal0 = np.random.random_sample((10000000,))
 pre_normal1 = np.random.random_sample((10000000,))
@@ -13,7 +14,9 @@ normal0 = pre_normal0 / np.sum(pre_normal0)
 normal1 = pre_normal1 / np.sum(pre_normal1)
 """
 
-print(timeit.timeit('mod.entropy_f64(normal0)', setup=setup_fut, number=100) * 10, "ms")
+print('Entropy (Futhark)', timeit.timeit('mod.entropy_f64(normal0)', setup=setup_fut, number=100) * 10, "ms")
+print('Scaled entropy (Futhark)', timeit.timeit('mod.entropy_scaled_f64(pre_normal0)', setup=setup_fut, number=100) * 10, "ms")
+print('Kullback-Liebler (Futhark)', timeit.timeit('mod.kullback_liebler_f64(normal0, normal1)', setup=setup_fut, number=100) * 10, "ms")
 
 setup_scipy = """
 from scipy.stats import entropy
@@ -21,8 +24,11 @@ from scipy.stats import entropy
 import numpy as np
 
 pre_normal0 = np.random.random_sample((10000000,))
+pre_normal1 = np.random.random_sample((10000000,))
 
 normal0 = pre_normal0 / np.sum(pre_normal0)
+normal1 = pre_normal1 / np.sum(pre_normal1)
 """
 
-print(timeit.timeit('entropy(normal0)', setup=setup_scipy, number=100) * 10, "ms")
+print('Entropy (scipy)', timeit.timeit('entropy(normal0)', setup=setup_scipy, number=100) * 10, "ms")
+print('Kullback-Liebler (scipy)', timeit.timeit('entropy(normal0, normal1)', setup=setup_scipy, number=100) * 10, "ms")
